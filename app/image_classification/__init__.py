2c1e7a38acb8023170d11738276528dcd10ce6d6
Qds11 <limqiqing96@gmail.com>
2024-07-24 11:54:14 +0800

    test: create image classification file

--- app/image_classification/__init__.py
+++ app/image_classification/__init__.py
@@ -1,6 +1,3 @@
-from flask import Blueprint
-
-image_classification_bp = Blueprint('image_classification', __name__)
-
-# Import routes to register them with the blueprint
-from . import routes
+version https://git-lfs.github.com/spec/v1
+oid sha256:eea2110d453560c177c3c78617dacb3f27595524db081dc20ee71ac8e84109c1
+size 179
--- app/image_classification/routes.py
+++ app/image_classification/routes.py
@@ -1,8 +1,3 @@
-from flask import request, jsonify
-from . import image_classification_bp
-
-@image_classification_bp.route('/', methods=['POST'])
-def classify_image():
-    # Add your image classification code here
-    print("hello")
-    return jsonify({'result': 'image classified'})
+version https://git-lfs.github.com/spec/v1
+oid sha256:36cd5567a4e4af70b60510f160eb32da1cf7fbe6ba39d100b22ac4c44e33f5d3
+size 3624
