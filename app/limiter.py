97b0a8fba2868833bbb2ecb3d7241f83eb5e0458
Qds11 <limqiqing96@gmail.com>
2024-07-31 09:52:39 +0800

    chore: switch to redis memory for rate limiting

--- app/limiter.py
+++ app/limiter.py
@@ -2,7 +2,11 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
 limiter = Limiter(
+    #  Retrieves the IP address of the client making the request
     key_func=get_remote_address,
-    #storage_uri='redis://redis:6379'
-    storage_uri='memory://'
+    # Redis is the backend for storing rate limit data
+    storage_uri='redis://redis:6379'
+
+    # local storage
+    #storage_uri='memory://'
 )
