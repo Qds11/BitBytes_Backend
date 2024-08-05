0e98ce24b6cad132d0372de49988074ab52ec108
Lim Qi Qing <101784318+Qds11@users.noreply.github.com>
2024-08-06 00:43:29 +0800

    feat: create clothes classification api

--- app/music_generation/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-music_generation_bp = Blueprint('music_generation', __name__)
-
-from . import routes
--- app/music_generation/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import request, jsonify
-from . import music_generation_bp
-
-@music_generation_bp.route('/', methods=['GET'])
-def classify_image():
-    # Add your image classification code here
-    return jsonify({'result': 'music generated'})
