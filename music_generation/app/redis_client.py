59cc1be5071fa50fe9099287faf2797987cdb8ae
Qds11 <limqiqing96@gmail.com>
2024-08-07 13:58:05 +0800

    chore: update yaml file

--- music_generation/app/redis_client.py
+++ music_generation/app/redis_client.py
@@ -1,6 +1,13 @@
 import redis
+from dotenv import load_dotenv
+import os
+load_dotenv()
 # Initialize Redis connection
-redis_client = redis.Redis(host='127.0.0.1', port=6379, db=0)
+redis_host = os.getenv('REDIS_HOST', 'localhost')
+redis_port = int(os.getenv('REDIS_PORT', 6379))
+redis_db = int(os.getenv('REDIS_DB', 0))
+
+redis_client = redis.Redis(host=redis_host, port=redis_port, db=redis_db)
 print("Redis Connection: ",redis_client)
 
 # Export the Redis client
